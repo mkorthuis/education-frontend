0e4b8e0689a5d2fadde43eb46b31a7c17f5c0ecf
Michael Korthuis <michael.korthuis@gmail.com>
2025-04-11 21:09:55 -0400

    Displayed Bullying Incidents.

--- src/features/district/components/safety/card/BullyCard.tsx
+++ src/features/district/components/safety/card/BullyCard.tsx
@@ -3,13 +3,25 @@ import { Box, Divider, Typography } from '@mui/material';
 import DefaultSafetyCard from './DefaultSafetyCard';
 import { FISCAL_YEAR } from '@/utils/environment';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
-import { selectSelectedSafetyPage, setSelectedSafetyPage } from '@/store/slices/safetySlice';
+import { selectDistrictBullyingData, selectStateBullyingData, selectSelectedSafetyPage, setSelectedSafetyPage } from '@/store/slices/safetySlice';
+import { selectCurrentDistrict } from '@/store/slices/locationSlice';
 
 const BullyCard: React.FC = () => {
     const dispatch = useAppDispatch();
+
+    const district = useAppSelector(selectCurrentDistrict);
+    const districtBullyingData = useAppSelector(state => selectDistrictBullyingData(state, {district_id: district?.id}));
+    const stateBullyingData = useAppSelector(state => selectStateBullyingData(state, {}));
+
     const selectedSafetyPage = useAppSelector(selectSelectedSafetyPage);
     const isSelected = selectedSafetyPage === 'bullying';
     
+    // Filter data for 2024 and calculate sums
+    const filtered2024Data = districtBullyingData.filter(item => item.year === 2024);
+    const actualSum = filtered2024Data.reduce((sum, item) => sum + (item.investigated_actual || 0), 0);
+    const reportedSum = filtered2024Data.reduce((sum, item) => sum + (item.reported || 0), 0);
+ 
+
     const handleClick = () => {
         dispatch(setSelectedSafetyPage('bullying'));
     };
@@ -23,10 +35,10 @@ const BullyCard: React.FC = () => {
         >
             <Box sx={{ my: 1 }}>
                 <Typography variant="body2" fontWeight="bold">
-                    X Bullying Incidents In {FISCAL_YEAR}
+                    {actualSum} Bullying Incidents In {FISCAL_YEAR}
                 </Typography>
                 <Typography variant="body2" color="text.secondary" sx={{ fontStyle: 'italic' }}>
-                    (Y Reported)
+                    ({reportedSum} Reported)
                 </Typography>
             </Box>
             <Divider sx={{ my: 1 }} />
