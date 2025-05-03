6e7a4bc416a7c307e35f9c157d9992ddecb428fe
Michael Korthuis <michael.korthuis@gmail.com>
2025-05-03 11:47:32 -0400

    Created a Temporary Footer.

--- src/components/layout/public/PublicLayout.tsx
+++ src/components/layout/public/PublicLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Box, useTheme, Card, Container, useMediaQuery } from "@mui/material";
 import Top from "./Top/Top";
+import Footer from "./Footer/Footer";
 import usePageTracking from "@/hooks/usePageTracking";
 
 const PrivateLayout = () => {
@@ -11,11 +12,12 @@ const PrivateLayout = () => {
   const isMediumOrLarger = useMediaQuery(theme.breakpoints.up('md'));
 
   const content = (
-    <Container maxWidth="xl" sx={{ py: isMediumOrLarger ? 2 : 0, px: isMediumOrLarger ? 2 : 0 }}>
+    <Container maxWidth="xl" sx={{ p : 0 }}>
         <Top />
-        <Box component="main" sx={{ flexGrow: 1, px: { xs: 2, md: 3 } }}>
+        <Box component="main" sx={{ flexGrow: 1, px: { xs: 2, md: 3 }, minHeight: '70vh' }}>
           <Outlet />
         </Box>
+        <Footer />
     </Container>
   );
 
